f81e3f86a825d50a6921ae513d7137c0546fb56e
satoNobu <nananiku.s.60@gmail.com>
2021-11-28 14:41:35 +0900

    バグを修正 #3

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
@@ -4,6 +4,7 @@
 package jp.co.yumemi.android.code_check
 
 import android.os.Parcelable
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import io.ktor.client.*
 import io.ktor.client.call.*
@@ -16,6 +17,7 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import kotlinx.parcelize.Parcelize
 import org.json.JSONObject
+import java.lang.Exception
 import java.util.*
 
 /**
@@ -27,37 +29,40 @@ class OneViewModel : ViewModel() {
     fun searchResults(inputText: String): List<Item> = runBlocking {
         val client = HttpClient(Android)
 
+        val items = mutableListOf<Item>()
         return@runBlocking GlobalScope.async {
-            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
-                header("Accept", "application/vnd.github.v3+json")
-                parameter("q", inputText)
-            }
+            try {
+                val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
+                    header("Accept", "application/vnd.github.v3+json")
+                    parameter("q", inputText)
+                }
 
-            val jsonBody = JSONObject(response.receive<String>())
+                val jsonBody = JSONObject(response.receive<String>())
 
-            val items = mutableListOf<Item>()
-            // アイテムの個数分ループする
-            jsonBody.optJSONArray("items")?.let {
-                for (i in 0 until it.length()) {
-                    it.optJSONObject(i)?.let { jsonItem ->
-                        items.add(
-                            Item(
-                                name = jsonItem.optString("full_name"),
-                                ownerIconUrl = jsonItem.optJSONObject("owner")?.optString("avatar_url") ?: "",
-                                language = jsonItem.optString("language"),
-                                stargazersCount = jsonItem.optLong("stargazers_count"),
-                                watchersCount = jsonItem.optLong("watchers_count"),
-                                forksCount = jsonItem.optLong("forks_conut"),
-                                openIssuesCount = jsonItem.optLong("open_issues_count")
+                // アイテムの個数分ループする
+                jsonBody.optJSONArray("items")?.let {
+                    for (i in 0 until it.length()) {
+                        it.optJSONObject(i)?.let { jsonItem ->
+                            items.add(
+                                Item(
+                                    name = jsonItem.optString("full_name"),
+                                    ownerIconUrl = jsonItem.optJSONObject("owner")?.optString("avatar_url") ?: "",
+                                    language = jsonItem.optString("language"),
+                                    stargazersCount = jsonItem.optLong("stargazers_count"),
+                                    watchersCount = jsonItem.optLong("watchers_count"),
+                                    forksCount = jsonItem.optLong("forks_conut"),
+                                    openIssuesCount = jsonItem.optLong("open_issues_count")
+                                )
                             )
-                        )
+                        }
                     }
                 }
+                lastSearchDate = Date()
+                return@async items.toList()
+            } catch (e: Exception) {
+                Log.i("ERROR: ", e.toString())
+                return@async items.toList()
             }
-
-            lastSearchDate = Date()
-
-            return@async items.toList()
         }.await()
     }
 }
